7d4136068eddd4382b8102fcb8566617ece6a595
Edward Chen <edwardchen805@gmail.com>
2018-07-06 00:10:02 -0700

    Naive Bayesian using scikit

--- Bayesian_Inference/Bayesian_Inference_Exercise.py
+++ Bayesian_Inference/Bayesian_Inference_Exercise.py
@@ -101,3 +101,52 @@ X_train, X_test, y_train, y_test = train_test_split(df['sms_message'],
 print('Number of rows in the total set: {}'.format(df.shape[0]))
 print('Number of rows in the training set: {}'.format(X_train.shape[0]))
 print('Number of rows in the test set: {}'.format(X_test.shape[0]))
+
+'''
+[Practice Node]
+
+The code for this segment is in 2 parts. Firstly, we are learning a vocabulary dictionary for the training data 
+and then transforming the data into a document-term matrix; secondly, for the testing data we are only 
+transforming the data into a document-term matrix.
+
+This is similar to the process we followed in Step 2.3
+
+We will provide the transformed data to students in the variables 'training_data' and 'testing_data'.
+'''
+
+# Instantiate the CountVectorizer method
+count_vector = CountVectorizer()
+
+# Fit the training data and then return the matrix
+training_data = count_vector.fit_transform(X_train)
+
+# Transform testing data and return the matrix. Note we are not fitting the testing data into the CountVectorizer()
+testing_data = count_vector.transform(X_test)
+
+'''
+Instructions:
+
+We have loaded the training data into the variable 'training_data' and the testing data into the 
+variable 'testing_data'.
+
+Import the MultinomialNB classifier and fit the training data into the classifier using fit(). Name your classifier
+'naive_bayes'. You will be training the classifier using 'training_data' and y_train' from our split earlier. 
+'''
+
+from sklearn.naive_bayes import MultinomialNB
+naive_bayes = MultinomialNB()
+naive_bayes.fit(training_data,y_train)
+
+'''
+Instructions:
+Now that our algorithm has been trained using the training data set we can now make some predictions on the test data
+stored in 'testing_data' using predict(). Save your predictions into the 'predictions' variable.
+'''
+
+predictions = naive_bayes.predict(testing_data)
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+print('Accuracy score: ', format(accuracy_score(y_test, predictions)))
+print('Precision score: ', format(precision_score(y_test, predictions)))
+print('Recall score: ', format(recall_score(y_test, predictions)))
+print('F1 score: ', format(f1_score(y_test, predictions)))
